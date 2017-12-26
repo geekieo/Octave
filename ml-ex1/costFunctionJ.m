9725aba2d2e89cf662ce55572c615fc3553a37f4
geekieo <geekieo@hotmail.com>
2017-12-27 01:07:25 +0800

    commit

---
+++ ml-ex1/costFunctionJ.m
@@ -0,0 +1,9 @@
+function J = costFunctionJ(X, y, theta)
+
+% X 是“经过设计”的训练样本矩阵，每行为一个特征样本
+% y 是类别标签，列向量（每行对应一个训练样本的标签）
+
+m = size(X,1);	%训练样本数，即 X 的行数
+predictions = X*theta;	%所有 m 个样本的多项式预测函数
+sqrErrors = (predictions -y).^2;	%平方误差矩阵
+J = 1/(2*m)*sum(sqrErrors);	%代价函数

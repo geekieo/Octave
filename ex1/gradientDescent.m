1dac19eb407806c57716f45c0e342c9b0f4e72d7
geekieo <geekieo@hotmail.com>
2018-01-09 10:50:14 +0800

    commit

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -16,8 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
+	H = X * theta; % size(H) should be (m,1)
+	theta =theta - alpha / m * ((H - y)' * X)';
+	
 
 
 
